45ebe9bb7ea60e3c56c6feaa3ce932ca5b940688
JavaBabyX <66321931+JavaBabyX@users.noreply.github.com>
2023-08-27 19:21:16 +0200

    Create helpersz.h

---
+++ basic img filter/helpersz.h
@@ -0,0 +1,17 @@
+#include "bmp.h"
+
+
+void copy(int height, int width, RGBTRIPLE source[height][width], RGBTRIPLE destination[height][width]);
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width]);
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width]);
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width]);
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width]);
+
